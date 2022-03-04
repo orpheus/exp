78784f1ff8636c4f976b8c6721a85251719c3c6e
ohryan <ryanachacon@gmail.com>
2022-03-04 11:11:47 -0700

    skillConfig FindById, CreateOne, CreateMany, DeleteById

--- controllers/skillconfig.go
+++ controllers/skillconfig.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"com.orpheus/exp/repository"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -15,10 +16,71 @@ func (s *SkillConfigController) RegisterRoutes() {
 	router := s.Router.Group("/api")
 	{
 		router.GET("/skillConfig", s.FindAllSkillConfigs)
+		router.GET("/skillConfig/:id", s.FindSkillConfigById)
+		router.POST("/skillConfig", s.CreateSkillConfig)
+		router.POST("/skillConfigs", s.CreateSkillConfigs)
+		router.DELETE("/skillConfig/:id", s.DeleteById)
 	}
 }
 
 func (s *SkillConfigController) FindAllSkillConfigs(c *gin.Context) {
 	skillConfigs := s.Repo.FindAll()
 	c.IndentedJSON(http.StatusOK, skillConfigs)
+}
+
+func (s *SkillConfigController) FindSkillConfigById(c *gin.Context) {
+	id := c.Param("id")
+	skillConfigs, err := s.Repo.FindById(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("database error: %s", err.Error())},
+		)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, skillConfigs)
+}
+
+func (s *SkillConfigController) CreateSkillConfig(c *gin.Context) {
+	var reqBody repository.SkillConfig
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	rec, err := s.Repo.CreateOne(reqBody)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("database error: %s", err.Error())},
+		)
+		return
+	}
+	c.JSON(http.StatusOK, rec)
+}
+
+func (s *SkillConfigController) CreateSkillConfigs(c *gin.Context) {
+	var reqBody []repository.SkillConfig
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println("Req: ", reqBody)
+	ret, err := s.Repo.CreateMany(reqBody)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error()},
+		)
+		return
+	}
+	c.JSON(http.StatusOK, ret)
+}
+
+func (s *SkillConfigController) DeleteById(c *gin.Context) {
+	id := c.Param("id")
+	response, err := s.Repo.DeleteById(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("database error: %s", err.Error())},
+		)
+		return
+	}
+	c.JSON(http.StatusOK, response)
 }
--- repository/skillconfig.go
+++ repository/skillconfig.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"log"
+	"strings"
 )
 
 type SkillConfig struct {
-	Id          string `json:"id"`
-	Name        string `json:"name"`
+	Id          string `json:"id" binding:"required"`
+	Name        string `json:"name" binding:"required"`
 	Description string `json:"description"`
 }
 
@@ -39,4 +40,50 @@ func (s *SkillConfigRepo) FindAll() []SkillConfig {
 
 	fmt.Println(skillConfigs)
 	return skillConfigs
+}
+
+func (s *SkillConfigRepo) FindById(id string) (SkillConfig, error) {
+	r := new(SkillConfig)
+	err := s.DB.QueryRow(
+		context.Background(),
+		"select * from skill_config where id = $1", id).
+		Scan(&r.Id, &r.Name, &r.Description)
+	return *r, err
+}
+
+func (s *SkillConfigRepo) CreateOne(skillConfig SkillConfig) (SkillConfig, error) {
+	fmtStr := "insert into skill_config (id, name, description) VALUES ('%s', '%s', '%s') RETURNING id, name, description"
+	sql := fmt.Sprintf(fmtStr, skillConfig.Id, skillConfig.Name, skillConfig.Description)
+	r := new(SkillConfig)
+	err := s.DB.QueryRow(context.Background(), sql).
+		Scan(&r.Id, &r.Name, &r.Description)
+
+	fmt.Println(r, err)
+	return *r, err
+}
+
+func (s *SkillConfigRepo) CreateMany(skillConfigs []SkillConfig) (string, error) {
+	// Create query string for insert many
+	sqlBase := "insert into skill_config (id, name, description) VALUES %s RETURNING id, name, description"
+	var sqlValues []string
+	for _, v := range skillConfigs {
+		col := fmt.Sprintf("('%s', '%s', '%s')", v.Id, v.Name, v.Description)
+		sqlValues = append(sqlValues, col)
+	}
+	sqlValuesJoined := strings.Join(sqlValues, ", ")
+	sql := fmt.Sprintf(sqlBase, sqlValuesJoined)
+
+	// Query db
+	sqlResult, err := s.DB.Exec(context.Background(), sql)
+	if err != nil {
+		return sqlResult.String(), err
+	}
+	return sqlResult.String(), nil
+}
+
+func (s *SkillConfigRepo) DeleteById(id string) (string, error) {
+	sql := fmt.Sprintf("delete from skill_config where id = '%s'", id)
+	fmt.Println(sql)
+	_, err := s.DB.Query(context.Background(), sql)
+	return fmt.Sprintf("Deleted SkillConfig with id: %s", id), err
 }
