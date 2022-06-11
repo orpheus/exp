4b150b186342fdb732ad0a2425ca289e62f99885
ohryan <ryanachacon@gmail.com>
2022-06-10 22:32:13 -0600

    add CORS

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgtype"
 	pgtypeuuid "github.com/jackc/pgtype/ext/gofrs-uuid"
@@ -12,6 +13,7 @@ import (
 	"github.com/orpheus/exp/db"
 	"log"
 	"os"
+	"time"
 )
 
 func getEnv(key, fallback string) string {
@@ -54,6 +56,15 @@ func main() {
 	db.Migrate(conn)
 
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"POST, OPTIONS, GET, PUT"},
+		AllowHeaders:     []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	controllers.RegisterAll(r, conn)
 
 	err = r.Run()
